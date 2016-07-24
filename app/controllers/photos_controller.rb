6faa9d3e91f43ad9b4f67dcc48ef54ed8fc011e5
Tomasz Więch <wm.tewu@gmail.com>
2016-07-24 14:34:38 +0200

    Added authorization checks in PhotosController and its views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,20 +1,14 @@
 class PhotosController < ApplicationController
-  before_action :set_album
-  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource :album
+  load_and_authorize_resource :photo, through: :album
 
 
   def show
-    @new_comment = Comment.new(flash[:new_comment])
+    @new_comment = @photo.comments.build(flash[:new_comment])
     @new_comment.valid? if flash[:new_comment]
   end
 
-  def new
-    @photo = Photo.new
-  end
-
   def create
-    @photo = Photo.new(photo_params)
-
     respond_to do |format|
       if @photo.save_and_add_to_album(@album)
         format.html { redirect_to [@album, @photo], notice: "Photo was successfully created." }
@@ -39,6 +33,7 @@ class PhotosController < ApplicationController
   end
 
   def destroy
+    @photo.albums.each { |album| authorize! :remove_photo, album }
     @photo.destroy
     respond_to do |format|
       format.html { redirect_to @album, notice: "Photo was successfully destroyed." }
@@ -48,14 +43,6 @@ class PhotosController < ApplicationController
 
 
   private
-
-  def set_album
-    @album = Album.find(params[:album_id])
-  end
-
-  def set_photo
-    @photo = Photo.find(params[:id])
-  end
 
   def photo_params
     params.require(:photo).permit(:image, :description)
