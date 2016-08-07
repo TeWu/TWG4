00eb4b617abb4f7611d8d4f18f12ae8e55abfa81
Tomasz Więch <wm.tewu@gmail.com>
2016-08-07 13:25:56 +0200

    Fixed JSON response in PhotosController

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,7 +15,7 @@ class PhotosController < ApplicationController
     respond_to do |format|
       if @photo.save
         format.html { redirect_to [@album, @photo], notice: "Photo was successfully created." }
-        format.json { render :show, status: :created, location: @photo }
+        format.json { render :show, status: :created, location: [@album, @photo] }
       else
         format.html { render :new }
         format.json { render json: @photo.errors, status: :unprocessable_entity }
@@ -27,7 +27,7 @@ class PhotosController < ApplicationController
     respond_to do |format|
       if @photo.update(photo_params)
         format.html { redirect_to [@album, @photo], notice: "Photo was successfully updated." }
-        format.json { render :show, status: :ok, location: @photo }
+        format.json { render :show, status: :ok, location: [@album, @photo] }
       else
         format.html { render :edit }
         format.json { render json: @photo.errors, status: :unprocessable_entity }
