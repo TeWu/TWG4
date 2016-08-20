5b7881255c4c612bdb0b35849e33e96c81e8093a
Tomasz Więch <wm.tewu@gmail.com>
2016-08-20 21:04:17 +0200

    Added control over notifications from JS

---
+++ lib/twg4/erb_helpers.rb
@@ -0,0 +1,13 @@
+module TWG4
+  module ERBHelpers
+    class << self
+
+      def render(partial_path, binding)
+        dir_name, _, partial_name = partial_path.rpartition(File::SEPARATOR)
+        file_name = "_#{partial_name}.html.erb"
+        Erubis::Eruby.new(File.read(File.join(Rails.root, 'app/views', dir_name, file_name)).gsub("'", %q(\\\'))).result(binding)
+      end
+
+    end
+  end
+end
