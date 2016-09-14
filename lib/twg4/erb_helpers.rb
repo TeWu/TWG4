c717db0c7b5fe9f2c24b0791acc9c72545c7d8c4
Tomasz Więch <wm.tewu@gmail.com>
2016-09-14 15:21:23 +0200

    Simple refactoring: Changed TWG4::ERBHelpers.render method to be less dependent on operating system

--- lib/twg4/erb_helpers.rb
+++ lib/twg4/erb_helpers.rb
@@ -5,7 +5,7 @@ module TWG4
       def render(partial_path, binding)
         dir_name, _, partial_name = partial_path.rpartition(File::SEPARATOR)
         file_name = "_#{partial_name}.html.erb"
-        Erubis::Eruby.new(File.read(File.join(Rails.root, 'app/views', dir_name, file_name)).gsub("'", %q(\\\'))).result(binding)
+        Erubis::Eruby.new(File.read(File.join(Rails.root, 'app', 'views', dir_name, file_name)).gsub("'", %q(\\\'))).result(binding)
       end
 
     end
