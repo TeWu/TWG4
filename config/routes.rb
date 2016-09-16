561a3d305d8a04ce99ac50a76edd90d1be8f2ee1
Tomasz Więch <wm.tewu@gmail.com>
2016-09-16 15:13:45 +0200

    Added redirection from '/albums/:id/photos' and from '/albums/:album_id/photos/:id/comments'

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,11 @@ Rails.application.routes.draw do
     resources :photos, only: :update, **json_only
     resources :photos, only: [:create, :destroy] do
       resources :comments, only: [:create, :edit, :update, :destroy]
+      get 'comments' => redirect('/albums/%{album_id}/photos/%{id}#comments-section'), on: :member
     end
 
     member do
+      get 'photos' => redirect('/albums/%{id}')
       delete 'remove-photo/:photo_id' => 'photos_in_albums#destroy', as: 'remove_photo_from'
       scope json_only do
         post 'add-photo' => 'photos_in_albums#create', as: 'add_photo_to'
