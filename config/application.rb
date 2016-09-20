2e703c069f8c4a47c8ef9299573f924b764a72a8
Tomasz Więch <wm.tewu@gmail.com>
2016-09-20 15:27:10 +0200

    Fixed production environment issues

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module Twg4
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Autoloading paths
-    config.autoload_paths += ['lib', 'config/abilities'].map { |p| config.root + p }
+    # Autoloading and eager loading paths
+    additional_load_paths = ['lib', 'config/abilities'].map { |p| config.root + p }
+    config.autoload_paths += additional_load_paths
+    config.eager_load_paths += additional_load_paths
   end
 end
